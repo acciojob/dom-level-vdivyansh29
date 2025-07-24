613d8e6f7082e2a883a57e705479d57cbc2c3be5
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-07-24 22:48:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function levelFinder(){
+	let currentElement = document.getElementById("level")
+	let count = 0
+
+	while(currentElement != null){
+		currentElement = currentElement.parentElement
+		count++
+	}
+
+	alert(`The level of the element is: ${count}`)
+}
+levelFinder()
